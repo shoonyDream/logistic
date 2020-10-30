f8d489b3a24faec399a5465527068059dcddecae
shoonyDream <y.vasytik@aevrika.ru>
2020-10-30 16:21:02 +0300

    remove id from hundler

--- src/components/custom-hundler/custom-hundler.js
+++ src/components/custom-hundler/custom-hundler.js
@@ -1,11 +1,21 @@
-var customHundler = document.getElementById("customHundler");
-var customHundlerValue = document.getElementById("customHundlerValue");
-customHundlerValue.innerHTML = customHundler.value;
+"use strict";
 
-customHundler.onchange = function() {
-  customHundlerValue.innerHTML = this.value;
-}
+document.addEventListener('DOMContentLoaded', function () {
+	var customHundlerContainer = document.querySelectorAll(".js-custom-hundler");
+	// блок с хандлером
 
-customHundler.oninput = function() {
-  customHundlerValue.innerHTML = this.value;
-}
+	customHundlerContainer.forEach(function (el) {
+		var customHundler = el.querySelector(".js-custom-hundler__input");
+		var customHundlerValue = el.querySelector('.js-custom-hundler__value span');
+
+		customHundlerValue.innerHTML = customHundler.value;
+
+		customHundler.onchange = function() {
+			customHundlerValue.innerHTML = this.value;
+		}
+
+		customHundler.oninput = function() {
+			customHundlerValue.innerHTML = this.value;
+		}
+	})
+});
